7588e8009ed36e0f65e81ab2d096c63b1d49b48c
what282 <robot0324@gmail.com>
2025-10-18 13:49:20 -0400

    created program.cs for entry point

--- src/Assembler/Program.cs
+++ src/Assembler/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Assembler;
+
+using Assembler.Core;
+using Assembler.Models;
+using Assembler.Exceptions;
+
+public class Program
+{
+    static Preprocessor preprocessor = new();
+    static Normalizer normalizer = new();
+    static Lexer lexer = new();
+    static Parser parser = new();
+
+    static int Main(string[] args)
+    {
+        if (args.Length is < 1 or > 2)
+        {
+            Console.Error.WriteLine("Usage: assembler <input.asm> [output.bin]");
+            return 1;
+        }
+        
+        string inputFile = args[0];
+        string? outputFile = args.Length > 1 ? args[1] : null;
+
+        string[] lines = File.ReadAllLines(inputFile);
+        string[] binary;
+
+        try
+        {
+            lines = preprocessor.PreprocessLines(lines, inputFile);
+            lines = normalizer.Normalize(lines);
+            Instruction[] instructions = lexer.GetInstructions(lines);
+            binary = parser.ParseInstructions(instructions);
+        }
+        catch(DirectiveException ex)
+        {
+            Console.WriteLine($"Directive error: {ex.Message}");
+            Console.WriteLine("Stop.");
+            return 1;
+        }
+        catch(FormatException ex)
+        {
+            Console.WriteLine($"Format error: {ex.Message}");
+            Console.WriteLine("Stop.");
+            return 1;
+        }
+        catch(SyntaxException ex)
+        {
+            Console.WriteLine($"Syntax error: {ex.Message}");
+            Console.WriteLine("Stop.");
+            return 1;
+        }
+        catch(SemanticException ex)
+        {
+            Console.WriteLine($"Semantic error: {ex.Message}");
+            Console.WriteLine("Stop.");
+            return 1;
+        }
+
+        if (outputFile != null)
+        {
+            File.WriteAllLines(outputFile, binary);
+            Console.WriteLine($"Output written to {outputFile}");
+        }
+        else
+        {
+            foreach (var line in binary)
+                Console.WriteLine(line);
+        }
+
+        return 0;
+    }
+}
