d8ff2112a5daaa567f20878dc08940486a92e085
what282 <robot0324@gmail.com>
2025-10-28 18:22:52 -0400

    added 'echo' example program

---
+++ examples/echo.asm
@@ -0,0 +1,13 @@
+#define handle_keyboard_address .kb_int
+
+PAGE0:
+    ldi r0, !0x08 
+    sys $sint, handle_keyboard_address
+loop:
+    nop
+    jmp .loop
+kb_int:
+    inp r1, @0
+    out @0, r1
+    iret
+
